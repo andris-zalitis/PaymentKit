c5aaf6b267d69f773dabbb0c38a2f59844aa4f3b
Yukan <yukanliao@gmail.com>
2014-12-15 00:14:38 -0500

    Appearance change. Remove becomefirstresponder.

--- PaymentKit/PTKView.m
+++ PaymentKit/PTKView.m
@@ -80,12 +80,15 @@ static NSString *const kPTKOldLocalizedStringsTableName = @"STPaymentLocalizable
     _isValidState = NO;
 
     self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, 290, 46);
-    self.backgroundColor = [UIColor clearColor];
-
-    UIImageView *backgroundImageView = [[UIImageView alloc] initWithFrame:self.bounds];
-    backgroundImageView.image = [[UIImage imageNamed:@"textfield"]
-            resizableImageWithCapInsets:UIEdgeInsetsMake(0, 8, 0, 8)];
-    [self addSubview:backgroundImageView];
+    self.backgroundColor = [UIColor colorWithWhite:0.f alpha:0.05];
+    [self.layer setCornerRadius:3.f];
+    [self.layer setBorderWidth:1.f];
+    [self.layer setBorderColor:[[UIColor colorWithWhite:0.2 alpha:0.5f] CGColor]];
+    
+//    UIImageView *backgroundImageView = [[UIImageView alloc] initWithFrame:self.bounds];
+//    backgroundImageView.image = [[UIImage imageNamed:@"textfield"]
+//            resizableImageWithCapInsets:UIEdgeInsetsMake(0, 8, 0, 8)];
+//    [self addSubview:backgroundImageView];
 
     self.innerView = [[UIView alloc] initWithFrame:CGRectMake(40, 12, self.frame.size.width - 40, 20)];
     self.innerView.clipsToBounds = YES;
@@ -97,15 +100,15 @@ static NSString *const kPTKOldLocalizedStringsTableName = @"STPaymentLocalizable
 
     [self.innerView addSubview:self.cardNumberField];
 
-    UIImageView *gradientImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 12, 34)];
-    gradientImageView.image = [UIImage imageNamed:@"gradient"];
-    [self.innerView addSubview:gradientImageView];
+//    UIImageView *gradientImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 12, 34)];
+//    gradientImageView.image = [UIImage imageNamed:@"gradient"];
+//    [self.innerView addSubview:gradientImageView];
 
-    self.opaqueOverGradientView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 9, 34)];
-    self.opaqueOverGradientView.backgroundColor = [UIColor colorWithRed:0.9686 green:0.9686
-                                                                   blue:0.9686 alpha:1.0000];
-    self.opaqueOverGradientView.alpha = 0.0;
-    [self.innerView addSubview:self.opaqueOverGradientView];
+//    self.opaqueOverGradientView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 9, 34)];
+//    self.opaqueOverGradientView.backgroundColor = [UIColor colorWithRed:0.9686 green:0.9686
+//                                                                   blue:0.9686 alpha:1.0000];
+//    self.opaqueOverGradientView.alpha = 0.0;
+//    [self.innerView addSubview:self.opaqueOverGradientView];
 
     [self addSubview:self.innerView];
     [self addSubview:self.placeholderView];
@@ -132,8 +135,10 @@ static NSString *const kPTKOldLocalizedStringsTableName = @"STPaymentLocalizable
     self.cardNumberField.delegate = self;
     self.cardNumberField.placeholder = [self.class localizedStringWithKey:@"placeholder.card_number" defaultValue:@"1234 5678 9012 3456"];
     self.cardNumberField.keyboardType = UIKeyboardTypeNumberPad;
-    self.cardNumberField.textColor = DarkGreyColor;
-    self.cardNumberField.font = DefaultBoldFont;
+    self.cardNumberField.textColor = [UIColor colorWithWhite:0.2 alpha:1.f];
+    //self.cardNumberField.textColor = DarkGreyColor;
+    self.cardNumberField.font = [UIFont boldSystemFontOfSize:15];
+    //self.cardNumberField.font = DefaultBoldFont;
 
     [self.cardNumberField.layer setMasksToBounds:YES];
 }
@@ -232,7 +237,7 @@ static NSString *const kPTKOldLocalizedStringsTableName = @"STPaymentLocalizable
                          }];
     }
 
-    [self.cardNumberField becomeFirstResponder];
+//    [self.cardNumberField becomeFirstResponder];
 }
 
 - (void)stateMeta
